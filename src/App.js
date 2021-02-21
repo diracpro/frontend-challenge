c8ab548cf2b07805e01ef8829848f19c301cc272
diracpro <eduardocastillosalgado@gmail.com>
2021-02-20 21:15:07 -0600

    Solution

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ const fetchOffersList = () => {
 export default function App() {
     return (
         <>
-            <p class="py-4 text-lg text-center">Start here :) Good luck!</p>
-
             <DirectoryList offerList={fetchOffersList()} />
         </>
     );
--- src/ui/components/DirectoryList.js
+++ src/ui/components/DirectoryList.js
@@ -1,5 +1,50 @@
+import MaterialTable from "material-table";
+const directoryUrl = "https://e6di35qzm7.execute-api.us-west-2.amazonaws.com/latest/directory";
 export default function DirectoryList({ offerList, ...props }) {
     log.debug("Here is your offers list", offerList);
-
-    return <></>;
+    
+    const [offerData, setOfferData] = useState({});
+    useEffect(() => {
+        getOfferDataWithFetch();
+    }, []);
+    const getOfferDataWithFetch = async () => {
+        fetch(directoryUrl)
+        .then(response => response.json())
+        .then(data => setOfferData(data.data))
+        .catch(e => { log.debug(e); });
+    };
+        
+    const arrayNames = Object.values(offerData).map(item => item.name);
+    const arrayDiscounts =  Object.values(offerData).map(item => item.discount);
+    var objectNames = arrayNames.map(arrayNames => ({name: arrayNames}));
+    var objectDiscounts = arrayDiscounts.map(arrayDiscounts => ({discount: arrayDiscounts}));
+    var arrayOfObjects = objectNames.map(function (value, index){
+       return [value, objectDiscounts[index]]
+    });
+    var directory = [];
+    arrayOfObjects.forEach(async arrayOf => {
+        directory.push(arrayOf.reduce(((r, c) => Object.assign(r, c)), {}));
+    });
+        
+    return (
+        <div className="App">
+            <header className="App-header"></header>
+            <div className="App">
+                <MaterialTable
+                columns={[
+                    { title: "Name", field: "name" },
+                    { title: "Discount", field: "discount" },
+                ]}
+                data={ directory }
+                title="OFFERINGS"
+                options={{
+                    isloading: true,
+                    exportButton: true,
+                    pageSize: 10,
+                    pageSizeOptions: [5,10,20,100]
+                  }}
+                />
+            </div>
+        </div>
+    );
 }
